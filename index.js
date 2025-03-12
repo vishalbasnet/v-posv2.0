0dff004630c60c35ad632c7739fee7bf01cee223
vishalbasnet <122615151+vishalbasnet@users.noreply.github.com>
2025-03-12 22:24:25 +0900

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('agree-checkbox').addEventListener('change', function() {
-    const agreeButton = document.getElementById('agree-button');
-    agreeButton.disabled = !this.checked;
-});
-
-document.getElementById('agree-button').addEventListener('click', function() {
-    window.location.href = 'redirect.html'; // Redirect to the next page
-});
